e2f11cb2d7b88d49053c0760fdd9b7e2cbd7b2b3
Ryan Palo <ryan@thepalos.com>
2018-05-04 18:15:38 -0700

    Get database straightened out and populate database with data

--- create_database.py
+++ create_database.py
@@ -1,26 +1,44 @@
 """Create schema and generate Sqlite3 Database"""
 
+import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy import Column, Integer, String, Date, Float
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
+engine = create_engine("sqlite:///hawaii.sqlite", echo=True)
+
 class Measurement(Base):
     """Hawaii data measurement"""
-    __tablename__ = 'measurements'
+    __tablename__ = 'measurement'
 
-    id = Column(Integer, primary_key=True)
+    measurement_id = Column(Integer, primary_key=True)
     station = Column(String)
-    date = Column(DateTime)
+    date = Column(Date)
     prcp = Column(Float)
     tobs = Column(Integer)
 
-def create_database(name):
-    filename = f"sqlite:///{name}.sqlite3"
-    engine = create_engine(filename, echo=True)
+class Station(Base):
+    """Hawaii weather stations"""
+    __tablename__ = 'station'
+
+    station_id = Column(Integer, primary_key=True)
+    station = Column(String)
+    name = Column(String)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    elevation = Column(Float)
+
+def create_database():
     Base.metadata.create_all(engine)
 
+def load_data():
+    measurements = pd.read_csv("clean_hawaii_measurements.csv")
+    stations = pd.read_csv("hawaii_stations.csv")
+    measurements.to_sql('measurement', engine, if_exists='append', index=False)
+    stations.to_sql('station', engine, if_exists='append', index_label='station_id')
+
 if __name__ == "__main__":
-    create_database("data")
+    create_database()
+    load_data()
